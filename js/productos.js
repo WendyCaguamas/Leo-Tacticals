ae861c98612e2049a132b8d3fb0496aa5133350e
WendyCaguamas <susana.rastrillo@gmail.com>
2026-08-15 16:32:01 -0500

    Update productos.js

--- js/productos.js
+++ js/productos.js
@@ -15,16 +15,19 @@ const productos = [
             "../img/001/001.jpeg",
             "../img/001/002.jpeg"
         ]
-    }
-
-],
+    },
 
     {
         id: 2,
+
         nombre: "Mochila Táctica",
+
         categoria: "equipamento",
+
         precio: 39.99,
+
         detalles: "Mochila táctica resistente para uso diario.",
+
         imagenes: [
             "../img/002/001.jpg",
             "../img/002/002.jpg",
@@ -34,10 +37,15 @@ const productos = [
 
     {
         id: 3,
+
         nombre: "Linterna Táctica",
+
         categoria: "otros",
+
         precio: 24.99,
+
         detalles: "Linterna compacta de alta resistencia.",
+
         imagenes: [
             "../img/003/001.jpg"
         ]
