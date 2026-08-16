7611d2dbb165805c25691c701529cebb2cadd6ab
WendyCaguamas <susana.rastrillo@gmail.com>
2026-08-16 14:50:33 -0500

    Update productos.js

--- js/productos.js
+++ js/productos.js
@@ -108,7 +108,7 @@ const productos = [
 
         detalles: "Disponible",
 
-        colores: ["verde", "azul"],
+        colores: ["negro", "verde"],
         imagenes: [
             "../img/007/001.jpeg",
             "../img/007/002.jpeg"
